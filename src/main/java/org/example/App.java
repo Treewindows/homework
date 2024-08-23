22cf68fedf495f33b08f34b91816c8e15c7502ae
Treewindows <vanyshkleda@mail.ru>
2024-08-23 22:46:29 +0300

    Поправил 3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -68,19 +68,29 @@ public class App {
      *
      *  Задание №6 Решить квадратное уравнение a*x^2 + b*x + c = 0 имея a,b,c ответ вывести в строку
      */
-    private static void discriminant(Double a, Double b, Double c) {
+    private static Double[] discriminant(Double a, Double b, Double c) {
+        Double [] array = new Double[2];
         Double D = ((Math.pow(b, 2)) - (4 * a * c));
         if (D > 0) {
             Double x1 = (-(b) + (Math.sqrt(D))) / (2 * a);
             Double x2 = (-(b) - (Math.sqrt(D))) / (2 * a);
-            System.out.print(String.format("Первый корень уравнения %s\n",x1));
-            System.out.print(String.format("Второй корень уравнения %s\n",x2));
+            array [0] = x1;
+            array [1] = x2;
+
+            //System.out.print(String.format("Первый корень уравнения %s\n",x1));
+            //System.out.print(String.format("Второй корень уравнения %s\n",x2));
         } else if (D < 0) {
             System.out.println("Нет действительных корней");
+            array [0] = null;
+            array [1] = null;
         } else  {
-            Double x12 = (-(b)) / (2 * a);
-            System.out.print(String.format("Корень уравнения %s\n",x12));
+            Double x = (-(b)) / (2 * a);
+            array [0] =x;
+            array [1] = null;
+            //System.out.print(String.format("Корень уравнения %s\n",x));
+
         }
+        return array;
     }
     public static void main(String[] args) {
         // Задание №1
@@ -100,6 +110,10 @@ public class App {
         String q =  triangle(4.0, 5.0, 6.0);
         System.out.print(q);
         // Задание №6
-        discriminant(16.0, -40.0, 25.0);
+        Double [] discriminant = discriminant(16.0, -40.0, -25.0);
+        System.out.print("Корень квадратного уравнения равен: ");
+        for (Integer i = 0; i < discriminant.length;i++){
+            System.out.printf("%.2f", discriminant[i]);
+        }
     }
 }
