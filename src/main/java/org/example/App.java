da1befd4cdb1ce4df111927a4306b52ab5be8bf7
Treewindows <vanyshkleda@mail.ru>
2024-09-22 18:58:40 +0300

    Логгеры alt+ctrl+L

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,34 @@
+
 package org.example;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.example.Person.Alexander;
+import org.example.Person.Boris;
+import org.example.Person.Dmitriy;
+import org.example.Person.Valeria;
+import org.example.Work.Doctor;
+import org.example.Work.Document;
+
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main(String[] args) {
+        Doctor doctor = new Doctor();
+        Document document = new Document();
+        Boris boris = new Boris();
+        Valeria valeria = new Valeria();
+        Dmitriy dmitriy = new Dmitriy();
+        Alexander alexander = new Alexander();
+        logger.info(boris);
+        logger.info(valeria);
+        logger.info(dmitriy);
+        logger.info(alexander);
+
+
     }
 }
