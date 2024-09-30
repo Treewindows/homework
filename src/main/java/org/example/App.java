c90361a1f4f55deb077f0018baf1db74714b4df4
Treewindows <vanyshkleda@mail.ru>
2024-09-30 21:22:01 +0300

    убрал лишнии конструкторы, строки пустые

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,14 +15,8 @@ public class App {
     public static void main(String[] args) {
         Person human = new Person("Дмитрий", "Денисов", "Юрьевич", 29, 1, 1970);
         Person human2 = new Person("Валерия", "Егорова", "Юрьевна", 23, 9, 1982);
-        Person human3 = new Person("Борис", "Исаев", "Анатольевич", "ЮАР", 5, 10, 1997);
-        Person human4 = new Person("Александр", "Осипов", "Иванович", 9, 3, 1975, 169, "Мужской");
-
-
         logger.info(human.toString());
         logger.info(human2.toString());
-        logger.info(human3.toString());
-        logger.info(human4.toString());
         logger.info("Результат сложения: {}", calculator.addition(2.2, 2.3));
         logger.info("Результат деления: {}", calculator.division(2.2, 2.3));
         logger.info("Результат умножения: {}", calculator.multiplication(2.2, 2.3));
